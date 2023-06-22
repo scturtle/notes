2900d82b25fdf7a0a4683080f4c99e8738139ae7
scturtle <scturtle@gmail.com>
2023-06-22 10:12:59 +0800

    stable div id

--- gen.sh
+++ gen.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 emacs \
 	--eval "(load-theme 'leuven t)" \
-	--eval "(global-tree-sitter-mode -1)" \
-	--eval '(mapc (lambda (f) (message f) (find-file f) (org-html-export-to-html) (kill-buffer))
-		(directory-files-recursively (expand-file-name ".") ".*\\.org$"))' \
-	--eval "(kill-emacs)"
+	--eval '(global-tree-sitter-mode -1)' \
+	--eval '(dolist (f (directory-files-recursively (expand-file-name ".") ".*\\.org$"))
+		(random "seed") (find-file f) (org-html-export-to-html) (kill-buffer))' \
+	--eval '(switch-to-buffer "*Messages*")' \
+	--eval '(if (y-or-n-p "exit?") (kill-emacs))'
