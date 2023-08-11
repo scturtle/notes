bbc9c05c677b771e021718b81df68c0fa1be97b8
scturtle <scturtle@gmail.com>
2023-08-11 19:25:49 +0800

    add mlir cheatsheet

--- gen.sh
+++ gen.sh
@@ -2,6 +2,9 @@
 emacs \
 	--eval "(load-theme 'leuven t)" \
 	--eval '(global-tree-sitter-mode -1)' \
+	--eval "(require 'diff-mode)" \
+	--eval "(set-face-attribute 'diff-added nil :foreground nil :background nil :inherit 'diff-indicator-added)" \
+	--eval "(set-face-attribute 'diff-removed nil :foreground nil :background nil :inherit 'diff-indicator-removed)" \
 	--eval '(dolist (f (directory-files-recursively (expand-file-name ".") ".*\\.org$"))
 		(random "seed") (find-file f) (org-html-export-to-html) (kill-buffer))' \
 	--eval '(switch-to-buffer "*Messages*")' \
